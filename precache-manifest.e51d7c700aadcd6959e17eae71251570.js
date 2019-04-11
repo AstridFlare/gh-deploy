8e441a454c8d8db501fa81e9cbbc8074eea51ec8
AstridFlare <AstridFlare@Gmail.com>
2019-04-10 22:30:23 -0700

    Updates

---
+++ precache-manifest.e51d7c700aadcd6959e17eae71251570.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-deploy/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b012f7ec3646cd44078e",
+    "url": "/gh-deploy/static/js/runtime~main.56966732.js"
+  },
+  {
+    "revision": "216d506b32345306810c",
+    "url": "/gh-deploy/static/js/main.d15b0620.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/gh-deploy/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "216d506b32345306810c",
+    "url": "/gh-deploy/static/css/main.4d6ada88.chunk.css"
+  },
+  {
+    "revision": "f6a7925fb2983dfc765f921b5f3218a9",
+    "url": "/gh-deploy/index.html"
+  }
+];
